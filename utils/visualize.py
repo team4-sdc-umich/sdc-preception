310f5e4057c50085597c12fc1ac55bf18da3e189
akaberto <akaberto@umich.edu>
2018-12-18 20:25:54 -0500

    Added tf record visualizer for convenience.

--- utils/visualize.py
+++ utils/visualize.py
@@ -1,3 +1,5 @@
+import matplotlib
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
@@ -7,7 +9,37 @@ from object_detection.data_decoders.tf_example_decoder import TfExampleDecoder a
 from google.protobuf import text_format
 
 
+
+global_exit_key = "H"
+global_exit = False
+d = {}
+axes = []
+c = []
+fig = []
+def press(event):
+    global global_exit
+    global global_exit_key
+    global axes
+    global c
+    global fig
+    key_pressed = event.key
+
+    if key_pressed == global_exit_key:
+        global_exit = True
+
+    if event.key in ["0", "1", "2"]:
+        d[axes[0].get_title()] = int(event.key)
+        c.set_text(key_pressed)
+        # axes[1].text(0.5, 0.5, "This is asasd")
+        fig.canvas.draw_idle()
+
+
 def visualize(tfrecords_filename, label_map=None):
+    global axes
+    global c
+    global fig
+    keep = 1
+    class_dict = {}
     if label_map is not None:
         label_map_proto = pb.StringIntLabelMap()
         with tf.gfile.GFile(label_map, 'r') as f:
@@ -21,6 +53,9 @@ def visualize(tfrecords_filename, label_map=None):
     for record in tf.python_io.tf_record_iterator(tfrecords_filename):
         example = decoder.decode(record)
         host_example = sess.run(example)
+        img_loc = host_example['source_id'].decode("utf-8")
+        if img_loc in d:
+            continue
         scores = np.ones(host_example['groundtruth_boxes'].shape[0])
         vu.visualize_boxes_and_labels_on_image_array(
             host_example['image'],
@@ -30,5 +65,32 @@ def visualize(tfrecords_filename, label_map=None):
             class_dict,
             max_boxes_to_draw=None,
             use_normalized_coordinates=True)
-        plt.imshow(host_example['image'])
+
+        keep = 1
+
+
+    
+        d[img_loc] = keep
+        fig, axes = plt.subplots(2, 1, figsize=(100, 100))
+        fig.canvas.mpl_connect('key_press_event', press)
+        axes[0].imshow(host_example['image'])
+        ax1 = axes[0].set_title(img_loc)
+        c = axes[1].text(0.5, 0.5, str(keep))
+        mng = plt.get_current_fig_manager()
         plt.show()
+        if global_exit:
+            break
+
+
+        return host_example
+    
+
+# scores = np.ones(host_example['groundtruth_boxes'].shape[0])
+# vu.visualize_boxes_and_labels_on_image_array(
+#             host_example['image'],
+#             host_example['groundtruth_boxes'],
+#             host_example['groundtruth_classes'],
+#             scores,
+#             class_dict,
+#             max_boxes_to_draw=None,
+#             use_normalized_coordinates=True)        
